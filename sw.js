b9a6e78655366515ee5293742c57bfa44a7e2469
Simon Haas <me@simonhaas.eu>
2020-11-22 18:03:33 +0000

    added installability

--- sw.js
+++ sw.js
@@ -4,8 +4,6 @@ const assets = [
     '/index.html',
     'https://code.jquery.com/jquery-3.5.0.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js',
-    'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
-    'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',
     'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'
 ]
 
